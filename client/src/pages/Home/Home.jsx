cba6074f7010ab26e47169e74c1a842babc8be9f
delwerhossain <delwerhossain006@gmail.com>
2023-07-12 16:16:53 +0100

    add: list insert on DB

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,24 +1,101 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 const Home = () => {
   const [hiddenField, setHiddenField] = useState(false);
-  
+  const [addList, setAddList] = useState("");
+  const [listData, setListData] = useState([]);
+
+  const mainList = (e) => {
+    e.preventDefault();
+    const list = e.target.list.value;
+
+    fetch("http://localhost:5000/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ list: list }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Request failed with status: " + response.status);
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        if (data) {
+          setAddList(list);
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/get")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Request failed with status: " + res.status);
+        }
+      })
+      .then((data) => setListData(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="text-center">
-      <h1 className="p-4  text-4xl my-3  font-bold border   rounded-lg">
+      <h1 className="p-4 text-4xl my-3 font-bold border rounded-lg">
         Add Your todo list
       </h1>
       <section className="">
-        <button onClick={() => setHiddenField(!hiddenField)} className="btn mb-6">
-          add Todo List
+        <button
+          onClick={() => setHiddenField(!hiddenField)}
+          className={`btn mb-6 ${hiddenField ? "btn-warning" : ""}`}
+        >
+          {!hiddenField ? "Add Todo List" : "Hidden List"}
         </button>
-        <div className={` flex justify-center items-center gap-4 ${!hiddenField && "hidden"}`}>
+        <form
+          onSubmit={mainList}
+          className={`flex justify-center items-center gap-4 mx-2 ${
+            hiddenField ? "hidden" : ""
+          }`}
+        >
           <input
+            name="list"
             type="text"
             placeholder="Type here"
             className="input input-bordered input-primary w-full max-w-xs"
           />
-          <button className="btn"> add</button>
+          <button type="submit" className="btn">
+            Add
+          </button>
+        </form>
+        {/* preview */}
+        <div
+          className={`border rounded-xl my-4 py-4 w-1/2 mx-auto ${
+            !addList ? "hidden" : ""
+          }`}
+        >
+          <ol>
+            {listData.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ol>
         </div>
       </section>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,17 +1,66 @@
 const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@simple-del.4ijtj0g.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    //collection
+    const userListCollection = client.db("todo-list").collection("user-list");
 
-// routes 
+    //--------------------------
+    //        API - Route
+    //--------------------------
+
+    // list related apis
+    app.get("/list", async (req, res) => {
+      const result = await userListCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/post", async (req, res) => {
+      const listData = req.body.list;
+      console.log(listData);
+      const result = await userListCollection.insertOne(listData);
+      res.send(result);
+    });
+
+    // ends
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// routes
 app.get("/", (req, res) => {
   res.send("simple CRUD");
 });
